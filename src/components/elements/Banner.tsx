1e7d0b57054d96afb2c293628e411785e036ef24
Ryan <rkim@greenwood.app>
2024-11-02 03:29:22 -0700

    added banner component

---
+++ src/components/elements/Banner.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import { cn, isExternal } from "@/lib/utils";
+
+interface Props {
+	className?: string;
+	content: React.ReactNode;
+	href: string;
+}
+
+export default function Banner({ className, content, href }: Props) {
+	return (
+		<Link
+			className={cn(
+				"flex h-20 w-screen items-center justify-center bg-zinc-950 text-center text-xl text-zinc-50 hover:opacity-90",
+				className,
+			)}
+			href={href}
+			{...(isExternal(href) && {
+				target: "_blank",
+				rel: "noopener noreferrer",
+			})}
+		>
+			{content}
+		</Link>
+	);
+}
