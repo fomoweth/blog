49f6a0af4a10cd1c870e65a6173714ac2d83c2ce
Ryan <rkim@greenwood.app>
2024-11-04 04:58:45 -0800

    removed unused packages and components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,16 +18,11 @@ const config: Config = {
 		},
 		extend: {
 			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
 				"fade-in":
 					"fade-in 1000ms var(--animation-delay, 0ms) ease forwards",
 				"fade-up":
 					"fade-up 1000ms var(--animation-delay, 0ms) ease forwards",
 				"infinite-scroll": "infinite-scroll 25s linear infinite",
-				marquee: "marquee var(--duration) infinite linear",
-				"marquee-vertical":
-					"marquee-vertical var(--duration) linear infinite",
 			},
 			borderRadius: {
 				sm: "calc(var(--radius) - 4px)",
@@ -108,29 +103,12 @@ const config: Config = {
 				"chart-5": "hsl(var(--chart-5))",
 			},
 			fontFamily: {
-				display: ["var(--font-orbiter)", "sans-serif"],
 				inter: ["var(--font-inter)", "sans-serif"],
 				orbiter: ["var(--font-orbiter)", "sans-serif"],
 				roboto: ["var(--font-roboto-flex)", "sans-serif"],
 				"roboto-mono": ["var(--font-roboto-mono)", "monospace"],
 			},
 			keyframes: {
-				"accordion-down": {
-					from: {
-						height: "0",
-					},
-					to: {
-						height: "var(--radix-accordion-content-height)",
-					},
-				},
-				"accordion-up": {
-					from: {
-						height: "var(--radix-accordion-content-height)",
-					},
-					to: {
-						height: "0",
-					},
-				},
 				"fade-in": {
 					from: { opacity: "0", transform: "translateY(-10px)" },
 					to: { opacity: "1", transform: "none" },
@@ -145,22 +123,6 @@ const config: Config = {
 					},
 					to: {
 						transform: "translateX(-100%)",
-					},
-				},
-				marquee: {
-					from: {
-						transform: "translateX(0)",
-					},
-					to: {
-						transform: "translateX(calc(-100% - var(--gap)))",
-					},
-				},
-				"marquee-vertical": {
-					from: {
-						transform: "translateY(0)",
-					},
-					to: {
-						transform: "translateY(calc(-100% - var(--gap)))",
 					},
 				},
 			},
