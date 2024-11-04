d831e27598eb702efe571da9eef26cbd33d8ac3b
Ryan <rkim@greenwood.app>
2024-11-04 07:14:07 -0800

    added metadata for home, blog, and projects pages

--- src/app/(client)/blog/page.tsx
+++ src/app/(client)/blog/page.tsx
@@ -1,6 +1,13 @@
 import View from "@/components/views/Blog";
 
+import { constructMetadata } from "@/lib/utils";
 import { loadCategories, loadPosts } from "@/sanity/lib/queries";
+
+export const metadata = constructMetadata({
+	title: "Blog",
+	description:
+		"Stay up-to-date with my latest projects and insights into smart contract engineering.",
+});
 
 export default async function Page() {
 	const [categories, posts] = await Promise.all([
--- src/app/(client)/projects/page.tsx
+++ src/app/(client)/projects/page.tsx
@@ -1,6 +1,12 @@
 import View from "@/components/views/Projects";
 
+import { constructMetadata } from "@/lib/utils";
 import { loadAuthor, loadProjects, loadProtocols } from "@/sanity/lib/queries";
+
+export const metadata = constructMetadata({
+	title: "Portfolio",
+	description: "Explore my hands-on work in smart contract development.",
+});
 
 export default async function Page() {
 	const [author, projects, protocols] = await Promise.all([
