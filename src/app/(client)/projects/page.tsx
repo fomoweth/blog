f0b7b3474a1bf47a20b7c4ee625c018b2a1f58f7
Ryan <rkim@greenwood.app>
2024-11-03 23:11:22 -0800

    updated projects page

--- src/app/(client)/projects/page.tsx
+++ src/app/(client)/projects/page.tsx
@@ -1,12 +1,13 @@
 import View from "@/components/views/Projects";
 
-import { loadAuthor, loadProjects } from "@/sanity/lib/queries";
+import { loadAuthor, loadProjects, loadProtocols } from "@/sanity/lib/queries";
 
 export default async function Page() {
-	const [author, projects] = await Promise.all([
+	const [author, projects, protocols] = await Promise.all([
 		loadAuthor(),
 		loadProjects(),
+		loadProtocols(),
 	]);
 
-	return <View author={author} projects={projects} />;
+	return <View author={author} projects={projects} protocols={protocols} />;
 }
